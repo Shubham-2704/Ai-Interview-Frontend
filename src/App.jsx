d663cb0480ac84d40cf0a6f5474bcf1f5359bcb4
Shubham-2704 <shubhambadresiya@gmail.com>
2026-02-20 23:20:27 +0530

    fix: correct import path for QuizHistoryPage in App component

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import SessionResources from "./pages/Admin/SessionResources";
 import QuizPage from "./pages/Quiz/QuizPage";
 import QuizHistory from "./pages/Quiz/QuizHistory";
 import QuizAnalytics from "./pages/Quiz/QuizAnalytics";
-import QuizHistoryPage from "./pages/admin/QuizHistoryPage";
+import QuizHistoryPage from "./pages/Admin/QuizHistoryPage";
 import { SettingsProvider } from "./context/SettingsContext";
 import SocketProvider from "./context/SocketContext";
 
